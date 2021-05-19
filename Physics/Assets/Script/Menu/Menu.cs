9cd1bcb12b693d1beb0ca4ff99960b568b7ec4b2
Jarosloov <jarosloov@yandex.ru>
2021-05-19 20:28:53 +0500

    Добавление меню

---
+++ Physics/Assets/Script/Menu/Menu.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private GameObject settings;
+    [SerializeField] private GameObject language;
+    [SerializeField] private GameObject menu;
+
+    [SerializeField] private Text lang;
+
+    private void Start()
+    {
+        settings.SetActive(false);
+        language.SetActive(false);
+        menu.SetActive(true);
+    }
+
+    public void Settings()
+    {
+        settings.SetActive(true);
+        menu.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        settings.SetActive(false);
+        language.SetActive(false);
+        menu.SetActive(true);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Language()
+    {
+        language.SetActive(true);
+        menu.SetActive(false);
+        settings.SetActive(false);
+    }
+
+    public void VariantLanguage(int numer)
+    {
+        if (numer == 0)
+        {
+            Debug.Log("Русский");
+            lang.text = "< Язык >";
+        }
+
+        if (numer == 1)
+        {
+            Debug.Log("ИНГЛИШЬ");
+            lang.text = "< language >";
+        }
+    }
+}
